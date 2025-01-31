6f985d2248a2b3148b589ca0359170a288f61394
Grommzy <grommzy@gmail.com>
2025-01-31 21:58:24 +1100

    Update to Styling

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function()
+{
     const header = document.getElementById("main_header");
 
-    if (window.scrollY > 50) {
+    if (window.scrollY > 50)
+    {
         header.classList.add("scrolled");
-    } else {
+    }
+    else
+    {
         header.classList.remove("scrolled");
     }
+});
+
+window.addEventListener("scroll", function ()
+{
+    let scrollY = window.scrollY;
+    let parallaxElement = document.querySelector(".Parallax");
+    
+    let speedFactor = 0.5; // Change this for different speeds (0.5 = half speed)
+    parallaxElement.style.transform = `translate(0, ${scrollY * speedFactor}px)`;
 });

816363667bfda591b450fb2a3ec97e7e98404c10
Grommzy <grommzy@gmail.com>
2025-02-01 15:24:19 +1100

    Introduced functionality for programming suite elements

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Sticky header
 window.addEventListener("scroll", function()
 {
     const header = document.getElementById("main_header");
@@ -12,6 +13,7 @@ window.addEventListener("scroll", function()
     }
 });
 
+// Background Parallax
 window.addEventListener("scroll", function ()
 {
     let scrollY = window.scrollY;
@@ -20,3 +22,19 @@ window.addEventListener("scroll", function ()
     let speedFactor = 0.5; // Change this for different speeds (0.5 = half speed)
     parallaxElement.style.transform = `translate(0, ${scrollY * speedFactor}px)`;
 });
+
+// Programming Elements Expansion/Collapsing
+document.querySelectorAll('.Language_Element').forEach(item => {
+    const info = item.querySelector('.Language_Description');
+  
+    // Expand on mouseenter
+    item.addEventListener('mouseenter', () => {
+      // Set the height to the scrollHeight (the full height of the content)
+      info.style.height = info.scrollHeight + 'px';
+    });
+  
+    // Collapse on mouseleave
+    item.addEventListener('mouseleave', () => {
+      info.style.height = '0';
+    });
+  });
